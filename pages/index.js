cae643390a833f4ab174fdceef626c8333feb231
ChriPi646 <christof@scarlet.be>
2025-05-27 22:40:39 +0200

    complete upload

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,1 @@
-// pages/index.js
-
-export default function Home() {
-  return (
-    <main style={{ padding: '2rem', fontFamily: 'Arial' }}>
-      <h1>Welkom bij Digestica</h1>
-      <p>Klik bovenaan op Login om te starten.</p>
-    </main>
-  );
-}
+// Homepagina code
