976655c0a7ca25c88b93c0636635a7061386825b
ChriPi646 <christof@scarlet.be>
2025-06-07 19:24:39 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,15 @@
 // Homepagina met knoppen
+
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Welkom bij The Belly Dr.</h1>
+      <p>Kies een van de opties hieronder:</p>
+      <div style={{ marginTop: "1rem" }}>
+        <button style={{ margin: "1rem" }}>Complete vragenlijst</button>
+        <button style={{ margin: "1rem" }}>Gerichte vragenlijst</button>
+        <button style={{ margin: "1rem" }}>Klachtendagboek</button>
+      </div>
+    </div>
+  );
+}
